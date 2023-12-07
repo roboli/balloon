2403aec2f061d591a995564bd43c2b518df4e2a1
roboli <robertooliveros@mac.com>
2023-12-07 17:11:51 -0600

    Fix assoc-inth for sequences

--- src/flato/core.clj
+++ src/flato/core.clj
@@ -104,8 +104,8 @@
         (if (and (map? val-found)
                  (seq rest-path))
           (assoc-in nm path-found (merge-with into val-found (assoc-inth {} rest-path v)))
-          (assoc-in nm path v)))
-      (assoc-in nm path v))))
+          (assoc-inth nm path v)))
+      (assoc-inth nm path v))))
 
 (defn inflate
   "Converts a one level deep flat map into a nested one."
