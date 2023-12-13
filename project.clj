76c48edc0509b149d5fb60a43ff453f2902ee2f4
roboli <robertooliveros@mac.com>
2023-12-13 16:10:05 -0600

    Add org.clojars.roboli group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject balloon "0.1.0-SNAPSHOT"
+(defproject org.clojars.roboli/balloon "0.1.0-SNAPSHOT"
   :description "Flat a nested map entry into one level deep, or inflate a one level map with delimited keys into a nested one"
   :url "https://github.com/roboli/balloon"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
