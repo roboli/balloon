f448e79790c435d24920e2865a0f00e2b3ed861e
roboli <robertooliveros@mac.com>
2023-12-20 17:16:57 -0600

    Bump 0.1.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject org.clojars.roboli/balloon "0.1.0-SNAPSHOT"
-  :description "Flat a nested map entry into one level deep, or inflate a one level map with delimited keys into a nested one"
+(defproject org.clojars.roboli/balloon "0.1.0"
+  :description "Deflate a nested map into one level deep or inflate a one level into a nested map using delimited keys"
   :url "https://github.com/roboli/balloon"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
